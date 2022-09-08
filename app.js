6892a4ac44b8da5fae5629c7697b4a35f98af52e
Iso-Doss <dossou.israel48@gmail.com>
2022-09-08 13:24:35 +0100

    Add ressources swagger

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.use('/api/auth', userRoutes);
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+//https://levelup.gitconnected.com/how-to-add-swagger-ui-to-existing-node-js-and-express-js-project-2c8bad9364ce
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
